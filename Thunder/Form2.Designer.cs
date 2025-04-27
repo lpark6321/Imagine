7b38f6092101052700130cd4db7c4fd1ae1694e3
lpark <lpark@DESKTOP-INP1OVT>
2025-04-28 01:11:45 +0800

    Grad功能解決

--- Thunder/Form2.Designer.cs
+++ Thunder/Form2.Designer.cs
@@ -44,9 +44,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
-            this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.Name = "Form2";
             this.Text = "Form2";
             this.ResumeLayout(false);
